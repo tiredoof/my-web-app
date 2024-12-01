17b7244bdd0c5e389b2fad91ca0c4ce135f7b7ca
may <mayq162001@gmail.com>
2024-12-01 20:51:11 +0100

    Add about page

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Docker!"
+@app.route("/about")
+def about():
+    return "About Docker and Git!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
